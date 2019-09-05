fa9a4ef7640c94e9aa321dd753d216b7de03e772
helinjiang <onehlj@qq.com>
2019-09-05 20:01:11 +0800

    feat: 升级到 0.1.2，获取js模块的内容

--- src/util/javascript.js
+++ src/util/javascript.js
@@ -1,18 +1,15 @@
 /**
  * 获取文件内容
  *
- * @param {String} filePath 文件路径
- * @param {Object} [inputParams] 蓝盾体系内的一些参数变量
- * @param {Object} [nodejsAtomSdk] 蓝盾的 nodejs 版本 sdk
+ * @param {*} jsModule 文件路径
+ * @param {Array} props 其他额外的参数
  * @return {Promise}
  * @author linjianghe
  */
-function read(filePath, inputParams, nodejsAtomSdk) {
+function getResult(jsModule, ...props) {
     return new Promise((resolve, reject) => {
-        const jsModule = require(filePath);
-
         // 有可能是 plain object ，也可能是函数，也可能函数里面是返回 Promise 的
-        const config = (typeof jsModule === 'function') ? jsModule(inputParams, nodejsAtomSdk) : jsModule;
+        const config = (typeof jsModule === 'function') ? jsModule(...props) : jsModule;
 
         Promise.resolve(config)
             .then((data) => {
@@ -24,6 +21,29 @@ function read(filePath, inputParams, nodejsAtomSdk) {
     });
 }
 
+/**
+ * 获取文件内容
+ *
+ * @param {String} filePath 文件路径
+ * @param {Array} props 其他额外的参数
+ * @return {Promise}
+ * @author linjianghe
+ */
+function read(filePath, ...props) {
+    return new Promise((resolve, reject) => {
+        const jsModule = require(filePath);
+
+        getResult(jsModule, ...props)
+            .then((data) => {
+                resolve(data);
+            })
+            .catch((err) => {
+                reject(err);
+            });
+    });
+}
+
 module.exports = {
-    read
+    read,
+    getResult
 };
