d427ebfc12ba621e4e8b497d5482398c8dab0add
linjianghe <linjianghe@tencent.com>
2020-07-11 12:07:42 +0800

    refactor: 使用 ts 改写

---
+++ src/evaluate/js-source.ts
@@ -0,0 +1,74 @@
+import path from 'path';
+import fse from 'fs-extra';
+
+import {read} from '../exec/javascript';
+import {runByExec} from '../run-cmd';
+
+export const EVALUATE_JS_SOURCE_TEXT_MODULE_TMP_PATH = path.join(__dirname, `../../tmp`);
+
+interface SourceOpts {
+  sourceText: string;
+  packageContent?: string;
+  NPM?: string;
+}
+
+/**
+ * 执行 js module 源码，获得执行结果
+ *
+ * @param {String|Object} sourceOpts
+ * @param {String} sourceOpts.sourceText js module 源码
+ * @param {String} sourceOpts.packageContent package.json的内容
+ * @param {String} sourceOpts.NPM 选择 npm 还是 cnpm等
+ * @param {Array} props 其他额外的参数
+ * @return {Promise}
+ * @author linjianghe
+ */
+export function evaluateJSSourceTextModule(sourceOpts: SourceOpts, ...props: any[]): Promise<any> {
+  let opts: SourceOpts;
+  if (sourceOpts && typeof sourceOpts === 'object') {
+    opts = sourceOpts;
+  } else {
+    opts = {
+      sourceText: sourceOpts,
+    };
+  }
+
+  const tmpSavePath = path.join(EVALUATE_JS_SOURCE_TEXT_MODULE_TMP_PATH, `t_${Date.now()}`);
+  const tmpSaveFilePath = path.join(tmpSavePath, `./code.js`);
+  const tmpPackageFilePath = path.join(tmpSavePath, `./package.json`);
+
+  return new Promise((resolve, reject) => {
+    if (opts.packageContent) {
+      fse.outputFileSync(tmpPackageFilePath, opts.packageContent);
+
+      runByExec(`${opts.NPM || 'npm'} install`, {cwd: tmpSavePath})
+        .then(data => {
+          resolve(data);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    } else {
+      resolve();
+    }
+  })
+    .then(() => {
+      // 保存
+      fse.outputFileSync(tmpSaveFilePath, opts.sourceText);
+
+      // 获取执行结果
+      return read(tmpSaveFilePath, ...props);
+    })
+    .then(data => {
+      // 清理
+      fse.removeSync(tmpSavePath);
+
+      return data;
+    })
+    .catch(err => {
+      // 清理
+      fse.removeSync(tmpSavePath);
+
+      return Promise.reject(err);
+    });
+}
